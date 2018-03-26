5e31de50799100a92ba8b39bc35bba7d1a348a49
jeetwide <wide.angle456@gmail.com>
2018-03-27 01:25:34 +0530

    initial commit

--- app/src/main/java/com/example/hplaptop/apidemo/MainActivity.java
+++ app/src/main/java/com/example/hplaptop/apidemo/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.hplaptop.apidemo;
 
+import android.app.ListActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -60,6 +61,15 @@ public class MainActivity extends AppCompatActivity {
                         
                         Log.d("TAG","success : "+ model2.success);
                         Toast.makeText(MainActivity.this, model2.success, Toast.LENGTH_SHORT).show();
+
+
+                        if(model2.success.equalsIgnoreCase("true")){
+                            recyclerView.setVisibility(View.VISIBLE);
+
+                            RecyclerAdapter recyclerAdapter = new RecyclerAdapter(MainActivity.this,model2.data);
+                            recyclerView.setAdapter(recyclerAdapter);
+
+                        }
                     }
 
                     @Override
@@ -71,4 +81,61 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+    public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder> {
+
+        Context ctx;
+        public ArrayList<SurveyModel> data;
+        public RecyclerAdapter(Context ctx, ArrayList<SurveyModel> temdata) {
+            this.ctx=ctx;
+            this.data=temdata;
+
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+            LayoutInflater inflater = LayoutInflater.from(ctx);
+            View view=inflater.inflate(R.layout.list_item,null);
+
+
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            SurveyModel surveyModel;
+
+            SurveyModel product= data.get(position);
+            holder.question_order.setText("question_order ="+product.question_order);
+            holder.options.setText("Options ="+product.options);
+            holder.question_id.setText("Question_Id = "+product.question_id);
+            holder.survey_title.setText("Survey_Title ="+product.survey_title);
+            holder.survey_id.setText("Survey_ID ="+product.survey_id);
+
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return data.size();
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            TextView survey_id,question,options,question_id,survey_title,question_order;
+
+            public ViewHolder(View itemView) {
+                super(itemView);
+
+                survey_id=itemView.findViewById(R.id.tv_survey_id);
+                options=itemView.findViewById(R.id.tv_options);
+                question=itemView.findViewById(R.id.tv_question);
+                question_id=itemView.findViewById(R.id.tv_question_id);
+                survey_title=itemView.findViewById(R.id.tv_survey_title);
+                question_order=itemView.findViewById(R.id.tv_question_order);
+            }
+
+
+        }
+    }
+
 }
